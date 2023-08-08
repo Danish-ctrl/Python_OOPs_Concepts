581d675d001ee41ad3b1e95014417a153a66dcd5
Danish <danish.reza1@gmail.com>
2023-08-08 06:24:40 +0200

    Fibonacci Series program updated.
    For Loop changed to function (fibonacci)

--- Loops/fabonacci.py
+++ Loops/fabonacci.py
@@ -3,13 +3,18 @@ Create a program that generates the Fibonacci sequence
 up to a given number of terms)
 """
 
+
+def fibonacci(first_num, second_num, num):
+    print('Fibonacci series are: ')
+    print(first_num, second_num, end=' ')
+    for i in range(1, num-1):
+        next_number = first_num + second_num
+        first_num = second_num
+        second_num = next_number
+        print(next_number, end=' ')
+
+
 first = int(input('Enter first number: '))
 second = int(input('Enter second number: '))
 n = 10
-print('Fibonacci series are: ')
-print(first, second, end=' ')
-for i in range(1, n-1):
-    next_number = first + second
-    first = second
-    second = next_number
-    print(next_number, end=' ')
+fibonacci(first, second, n)
